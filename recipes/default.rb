d337a55620abefe46e8e41ec11ca9990fea71755
Mike Doty <mdoty@bitpusher.com>
2018-02-12 12:57:51 -0700

    use chef retry logic to make up for rubygems failures

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,6 +21,8 @@ chef_gem 'aws-sdk' do
   version node['aws']['aws_sdk_version']
   compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
   action :install
+  retries 10
+  retry_delay 30
 end
 
 require 'aws-sdk'
